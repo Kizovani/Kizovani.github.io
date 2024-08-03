172509d650246bb46d4632aa8cb8c2436ee129ef
Kizovani <perailaki@gmail.com>
2024-08-02 23:34:05 -0400

    testing, green box working and excluded node_modules folder

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+// move the camera so it is not inside the box we made above
+camera.position.z = 5;
+ 
+// to actually see anything we need an animation loop, as seen below:
+function animate() {
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
