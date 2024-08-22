c528e25e1285d2954b51ac588e60eccdd858ea14
Kizovani <perailaki@gmail.com>
2024-08-22 01:18:04 -0400

    added vite config file

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        // Add more pages as needed
+      },
+    },
+  },
+})
