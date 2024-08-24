a6f6b42ab99b474b360db601041aa898a9feb812
Kizovani <perailaki@gmail.com>
2024-08-24 02:38:41 -0400

    completely Reorganized main.js, now divided up into shared and unique functions. Model now should be much easier to handle fixed color links, made project cards, change. If something broke it happened here lol.

--- main.js
+++ main.js
@@ -1,178 +1,68 @@
-//TODO: ORGANIZE THIS HOW CLAUDE SUGGESTED PLEASE
+// Shared functionality
 import baffle from 'baffle';
 
-//text handling
-document.addEventListener('DOMContentLoaded', function() {
-  
-  let nametag = baffle('.nametag', {
+// Menu functionality
+function initMenu() {
+  const tl = gsap.timeline({ paused: true });
 
-    characters: '▒██ ▒▒>>█ >▒<█< ▓▒▒ ▓▒█$▒▓ ▒░/█ ▓▒#▒ ░▓▒▒ >/░▒',
-    speed: 70,
-  });
+  function revealMenu() {
+    revealMenuItems();
+    const toggleBtn = document.getElementById("menu-toggle");
+    const closeBtn = document.getElementById("close-menu");
+    toggleBtn.onclick = function (e) {
+      tl.reversed(!tl.reversed());
+    };
+    closeBtn.onclick = function (e) {
+      tl.reversed(!tl.reversed());
+    };
+  }
 
-  nametag.start();
-  //small delay to allow the baffle to happen so you dont see "kizovani" flash before it is baffled
-  setTimeout(() => {
-    const welcomeText = document.querySelector('.nametag');
-    welcomeText.style.display = 'inline-block';
-  }, 50);
+  function revealMenuItems() {
+    tl.to(".menu-container", 0.01, {
+      height: "205px",
+    });
 
-  //reveal text when model is loaded
-  window.addEventListener('modelLoaded', function() {
-    
-  nametag.reveal(4000, 1700);
-  });
-
-  // TODO:
-  // maybe add a text where if they spin him around very rapidly he says something like "im getting dizzy"
-  // and then the second time something like please stop im going to throw up
-  // and then maybe a throw up effect? But like idk if I can make it cooler/different from the konami code?!?!?!?
-  // ideas for how to do above:
-  // log camera coords, if the change in camera coords is too drastic too fast, trigger
-  // or I could detect mouse movement and if when the user is holding and dragging left click too fast it triggers that way, I feel like that could be easier/more reliable, but idk if there
-  //even is a way to detect mouse "velocity"
-  // konami code can just be the baffle effect everywhere and then atr the end its the ultron quote "youve wounded me, full marks for that" (or some fuck shit like that)
-  //maybe incorporate a quote of the day thing, after all the intro messages he says like "Todays quote of the day is:", just worried its going to be too much text for some of the larger quotes
-
-const messages = [
-  { content: "Welcome", duration: 4000 },
-  { content: "move me using your mouse", duration: 3000 },
-  { content: "drag with left and right click for rotation and position", duration: 5000 },
-  { content: "zoom using the scroll wheel", duration: 4000 },
-  { content: "you can also play with my lighting effects..", duration: 5000 },
-  { content: "using the scroll bar for light rotation", duration: 5000 },
-  { content: "and the switch for ambient light", duration: 5000 },
-  { content: "check out the menu for more", duration: 4000 }
-];
-
-
-let currentIndex = 0;
-  let baffleEffect = baffle('.welcome-text', {
-
-    characters: '▒██ ▒▒>>█ >▒<█< ▓▒▒ ▓▒█$▒▓ ▒░/█ ▓▒#▒ ░▓▒▒ >/░▒',
-    speed: 70,
-  })
-
-  window.addEventListener('modelLoaded', function() {
-    const initialDelay = 4700; //4.7 second delay before anything else happens after model is loaded
-    setTimeout(function(){
-      const welcomeText = document.querySelector('.welcome-text');
-      welcomeText.style.display = 'inline-block';
-      displayNextMessage();
-    }, initialDelay);
-  });
-
-    function displayNextMessage() {
-      if (currentIndex >= messages.length) {
-        handleEndOfMessages();
-        return;
-      }
-
-      const currentMessage = messages[currentIndex];
-      baffleEffect.start();
-      baffleEffect.text(() => currentMessage.content).reveal(1500);
-
-      currentIndex++;
-
-      //schedule the next message or end of messages
-
-      setTimeout(() => {
-        if (currentIndex < messages.length) {
-          displayNextMessage();
-        } else {
-          handleEndOfMessages();
-        }
-      }, currentMessage.duration);
-    }
-    
-    //get rid of current message, wait some time, then display joke
-      function handleEndOfMessages() {
-        console.log("all messages displayed")
-
-        const welcomeText = document.querySelector('.welcome-text');
-        const timeToTextDissapear = 4000;
-        const timeToBaffleBeforeDissapear = 3500;
-        setTimeout(() => {
-          baffleEffect.start();
-        }, timeToBaffleBeforeDissapear);
-        setTimeout(() => {
-          welcomeText.style.display = 'none';
-        }, timeToTextDissapear);
-
-        const jokeTimeout = 120000; //2 minute delay before joke
-        setTimeout(() => {
-          baffleEffect.start();
-          welcomeText.style.display = 'inline-block';
-          baffleEffect.text(() => "why are you still here?").reveal(1000);
-        }, jokeTimeout);
-      }
-
-});
-
-
-
-//menu bar:
-//idea basically copied from codegrid "brutalist menu bar" so I have basically no idea how anything below this line works
-const tl = gsap.timeline({ paused: true });
-
-function revealMenu() {
-  revealMenuItems();
-  const toggleBtn = document.getElementById("menu-toggle");
-  const closeBtn = document.getElementById("close-menu");
-  toggleBtn.onclick = function (e) {
-    tl.reversed(!tl.reversed());
-  };
-  closeBtn.onclick = function (e) {
-    tl.reversed(!tl.reversed());
-  };
-}
-
-function revealMenuItems() {
-  tl.to(".menu-container", 0.01, {
-    //INCREASE THIS IF ITS CUTOFF AT THE BOTTOM OF THE LIST!!
-    height: "205px",
-  });
-
-  tl.to(".col-1", 1, {
-    left: "-200px",
-    ease: "power4.inOut",
-  });
-
-  tl.to(
-    ".col-2",
-    0.025,
-    {
-      left: "0px",
+    tl.to(".col-1", 1, {
+      left: "-200px",
       ease: "power4.inOut",
-    },
-    "<"
-  );
+    });
 
-  tl.to(
-    ".col-2 > .menu-item",
-    1,
-    {
-      left: 0,
-      ease: "power4.inOut",
-      stagger: {
-        //change this for more or less delay between each item
-        amount: 0.35,
+    tl.to(
+      ".col-2",
+      0.025,
+      {
+        left: "0px",
+        ease: "power4.inOut",
       },
-    },
-    "<"
-  ).reverse();
+      "<"
+    );
+
+    tl.to(
+      ".col-2 > .menu-item",
+      1,
+      {
+        left: 0,
+        ease: "power4.inOut",
+        stagger: {
+          amount: 0.35,
+        },
+      },
+      "<"
+    ).reverse();
+  }
+
+  revealMenu();
 }
 
-gsap.to(".marquee", {
-  x: "-25%",
-  duration: 10,
-  ease: "none",
-  repeat: -1,
-  yoyo: true,
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  revealMenu();
+// Marquee functionality
+function initMarquee() {
+  gsap.to(".marquee", {
+    x: "-25%",
+    duration: 10,
+    ease: "none",
+    repeat: -1,
+    yoyo: true,
+  });
 
   let menuContainer = document.querySelector(".menu-container");
   let marqueeContainer = document.querySelector(".marquee-container");
@@ -199,7 +89,6 @@ document.addEventListener("DOMContentLoaded", function () {
         scale: 1,
         left: containerX + 25,
         top: containerY,
-        //this is the duration of the "enlarging" animation into the marquee
         duration: 0.2,
         ease: "power3.out",
       });
@@ -207,7 +96,6 @@ document.addEventListener("DOMContentLoaded", function () {
       let hoveredMenuItem = event.target.closest(".menu-item");
       if (hoveredMenuItem) {
         let hoveredText = hoveredMenuItem.textContent.trim();
-        //increase this and the one below it if the scrolling text ends or reverses to quickly
         let marqueeContent = (hoveredText + " ").repeat(24);
         marqueeText.innerHTML = marqueeContent.replace(/\s/g, "&nbsp;");
       }
@@ -218,7 +106,6 @@ document.addEventListener("DOMContentLoaded", function () {
     isInsideMenuContainer = false;
     gsap.to(marqueeContainer, {
       scale: 0,
-      //this is the duration of the "shrinking" animation out of the marquee"
       duration: 0.2,
       ease: "power3.out",
       onComplete: function () {
@@ -229,4 +116,122 @@ document.addEventListener("DOMContentLoaded", function () {
     let marqueeContent = ("home" + " ").repeat(24);
     marqueeText.innerHTML = marqueeContent.replace(/\s/g, "&nbsp;");
   });
+}
+
+// Page-specific functionality
+
+// Home page
+function initHomePage() {
+  const messages = [
+    { content: "Welcome", duration: 4000 },
+    { content: "move me using your mouse", duration: 3000 },
+    { content: "drag with left and right click for rotation and position", duration: 5000 },
+    { content: "zoom using the scroll wheel", duration: 4000 },
+    { content: "you can also play with my lighting effects..", duration: 5000 },
+    { content: "using the scroll bar for light rotation", duration: 5000 },
+    { content: "and the switch for ambient light", duration: 5000 },
+    { content: "check out the menu for more", duration: 4000 }
+  ];
+
+  let currentIndex = 0;
+  let baffleEffect = baffle('.welcome-text', {
+    characters: '▒██ ▒▒>>█ >▒<█< ▓▒▒ ▓▒█$▒▓ ▒░/█ ▓▒#▒ ░▓▒▒ >/░▒',
+    speed: 70,
+  });
+
+  function displayNextMessage() {
+    if (currentIndex >= messages.length) {
+      handleEndOfMessages();
+      return;
+    }
+
+    const currentMessage = messages[currentIndex];
+    baffleEffect.start();
+    baffleEffect.text(() => currentMessage.content).reveal(1500);
+
+    currentIndex++;
+
+    setTimeout(() => {
+      if (currentIndex < messages.length) {
+        displayNextMessage();
+      } else {
+        handleEndOfMessages();
+      }
+    }, currentMessage.duration);
+  }
+
+  function handleEndOfMessages() {
+    console.log("all messages displayed");
+
+    const welcomeText = document.querySelector('.welcome-text');
+    const timeToTextDissapear = 4000;
+    const timeToBaffleBeforeDissapear = 3500;
+    setTimeout(() => {
+      baffleEffect.start();
+    }, timeToBaffleBeforeDissapear);
+    setTimeout(() => {
+      welcomeText.style.display = 'none';
+    }, timeToTextDissapear);
+
+    const jokeTimeout = 120000; // 2 minute delay before joke
+    setTimeout(() => {
+      baffleEffect.start();
+      welcomeText.style.display = 'inline-block';
+      baffleEffect.text(() => "why are you still here?").reveal(1000);
+    }, jokeTimeout);
+  }
+
+  window.addEventListener('modelLoaded', function() {
+    const initialDelay = 4700; // 4.7 second delay before anything else happens after model is loaded
+    setTimeout(function(){
+      const welcomeText = document.querySelector('.welcome-text');
+      welcomeText.style.display = 'inline-block';
+      displayNextMessage();
+    }, initialDelay);
+  });
+
+  let nametag = baffle('.nametag', {
+    characters: '▒██ ▒▒>>█ >▒<█< ▓▒▒ ▓▒█$▒▓ ▒░/█ ▓▒#▒ ░▓▒▒ >/░▒',
+    speed: 70,
+  });
+
+  nametag.start();
+  setTimeout(() => {
+    const welcomeText = document.querySelector('.nametag');
+    welcomeText.style.display = 'inline-block';
+  }, 50);
+
+  window.addEventListener('modelLoaded', function() {
+    nametag.reveal(5000, 2000);
+  });
+}
+
+// Projects page
+function initProjectsPage() {
+  // Add any projects page specific functionality here
+  console.log("Projects page initialized");
+}
+
+// Main initialization
+document.addEventListener('DOMContentLoaded', function() {
+  initMenu();
+  initMarquee();
+
+  // Determine which page we're on and initialize accordingly
+  if (document.querySelector('.nametag')) {
+    initHomePage();
+  } else if (document.getElementById('projects-container')) {
+    initProjectsPage();
+  }
 });
+
+// TODO:
+  // maybe add a text where if they spin him around very rapidly he says something like "im getting dizzy"
+  // and then the second time something like please stop im going to throw up
+  // and then maybe a throw up effect? But like idk if I can make it cooler/different from the konami code?!?!?!?
+  // ideas for how to do above:
+  // log camera coords, if the change in camera coords is too drastic too fast, trigger
+  // or I could detect mouse movement and if when the user is holding and dragging left click too fast it triggers that way, I feel like that could be easier/more reliable, but idk if there
+  //even is a way to detect mouse "velocity"
+  // konami code can just be the baffle effect everywhere and then atr the end its the ultron quote "youve wounded me, full marks for that" (or some fuck shit like that)
+  //maybe incorporate a quote of the day thing, after all the intro messages he says like "Todays quote of the day is:", just worried its going to be too much text for some of the larger quotes
--- three-scene.js
+++ three-scene.js
@@ -2,6 +2,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { SimplifyModifier } from 'three/examples/jsm/modifiers/SimplifyModifier.js';
 import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
@@ -17,8 +18,10 @@ let rotatingLight;
 let ambientLight;
 
 function initThreeScene() {
+
+    //set up camera, lighting, renderer, and effect
     
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.03 , 1000)
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01 , 1000);
     camera.lookAt(0 , 0 , 0);
     camera.position.set(0 , 0 , 0.15);
 
@@ -69,7 +72,8 @@ function initThreeScene() {
     createLoadingText();
 
     //load loading text and model
-    loadModel();
+    // loadModel();
+    loadModelBasic();
 
     //start animation loop
     animate();
@@ -144,78 +148,80 @@ function createLoadingText() {
 //     }
 // }
 
-function loadModel() {
-    const loader = new GLTFLoader();
 
-    //simple material to use for all meshes instead of bundled materials:
-    const simpleMaterial = new THREE.MeshPhongMaterial({
-color: 0xcccccc,  // Light gray color
-flatShading: true  // This gives a more polygon-like appearance
-});
+// //old code (i hope the new one works) (it does i need to remove this soon, leaving it here for the time being sicne it is not harming anyone)
+// function loadModel() {
+//     const loader = new GLTFLoader();
 
-    loader.load(
-        //full size (full poly count) model:
-        //'models/scene.gltf',
-        // //low poly model (load times a bit better and alot less system intensive, might lower model count more, also please change this file name lmfao):
-        //'res/models/lowpoly_textures/test3/test0.2then0.5then0.25.gltf',
-        //model with no "backpack" since the backpack was interfering when I wanted to mesh it with a blank mesh:
-        'res/models/NEW ANGEL MODEL NO BACKPACK/no backpack.gltf',
-        function(gltf) {
-            console.log('Model loaded successfully');
-            model = gltf.scene;
+//     //simple material to use for all meshes instead of bundled materials:
+//     const simpleMaterial = new THREE.MeshPhongMaterial({
+// color: 0xcccccc,  // Light gray color
+// flatShading: true  // This gives a more polygon-like appearance
+// });
 
-            // Set the material for all meshes in the model
-            model.traverse((child) => {
-                if (child.isMesh) {
-                    // Remove any texture-related properties
-                    child.material = simpleMaterial.clone();  // Clone to allow for individual adjustments if needed
-                    child.material.map = null;
-                    child.material.normalMap = null;
-                    child.material.specularMap = null;
-                    child.material.alphaMap = null;
-                }
-            });
+//     loader.load(
+//         //full size (full poly count) model:
+//         //'models/scene.gltf',
+//         // //low poly model (load times a bit better and alot less system intensive, might lower model count more, also please change this file name lmfao):
+//         //'res/models/lowpoly_textures/test3/test0.2then0.5then0.25.gltf',
+//         //model with no "backpack" since the backpack was interfering when I wanted to mesh it with a blank mesh:
+//         'res/models/NEW ANGEL MODEL NO BACKPACK/no backpack.gltf',
+//         function(gltf) {
+//             console.log('Model loaded successfully');
+//             model = gltf.scene;
+
+//             // Set the material for all meshes in the model
+//             model.traverse((child) => {
+//                 if (child.isMesh) {
+//                     // Remove any texture-related properties
+//                     child.material = simpleMaterial.clone();  // Clone to allow for individual adjustments if needed
+//                     child.material.map = null;
+//                     child.material.normalMap = null;
+//                     child.material.specularMap = null;
+//                     child.material.alphaMap = null;
+//                 }
+//             });
             
-            scene.add(model);
+//             scene.add(model);
 
-            // Center the model
-            const box = new THREE.Box3().setFromObject(model);
-            const center = box.getCenter(new THREE.Vector3());
-            model.position.sub(center);
+//             // Center the model
+//             const box = new THREE.Box3().setFromObject(model);
+//             const center = box.getCenter(new THREE.Vector3());
+//             model.position.sub(center);
 
-            // Scale the model if it's too small or too large
-            const scaleAdjustment = 6; // Adjust this value as needed
-            model.scale.multiplyScalar(scaleAdjustment);
+//             // Scale the model if it's too small or too large
+//             const scaleAdjustment = 6; // Adjust this value as needed
+//             model.scale.multiplyScalar(scaleAdjustment);
 
-            console.log('Model position:', model.position);
-            console.log('Model scale:', model.scale);
+//             console.log('Model position:', model.position);
+//             console.log('Model scale:', model.scale);
 
-            model.position.y -=0.11; //moving the model down a bit
+//             model.position.y -=0.11; //moving the model down a bit
 
-            // Set up animation
-            if (gltf.animations && gltf.animations.length > 0) {
-                mixer = new THREE.AnimationMixer(model);
-                const action = mixer.clipAction(gltf.animations[0]);
-                action.play();
-            }
+//             // Set up animation
+//             if (gltf.animations && gltf.animations.length > 0) {
+//                 mixer = new THREE.AnimationMixer(model);
+//                 const action = mixer.clipAction(gltf.animations[0]);
+//                 action.play();
+//             }
 
-            // Remove loading text
-            if (loadingText) {
-                scene.remove(loadingText);
-            }
+//             // Remove loading text
+//             if (loadingText) {
+//                 scene.remove(loadingText);
+//             }
 
-            // dispatch a custom event to trigger the text reveal
-            window.dispatchEvent(new Event('modelLoaded'));
+//             // dispatch a custom event to trigger the text reveal
+//             window.dispatchEvent(new Event('modelLoaded'));
 
-        },
-        function(xhr) {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        function(error) {
-            console.error('An error happened', error);
-        }
-    );
-}
+//         },
+//         function(xhr) {
+//             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+//         },
+//         function(error) {
+//             console.error('An error happened', error);
+//         }
+//     );
+// }
 
 function animate() {
     
@@ -246,3 +252,87 @@ document.addEventListener('DOMContentLoaded', () => {
     initThreeScene();
     initDOMControls();
 });
+
+
+function loadModelBasic() {
+    const loader = new GLTFLoader();
+
+    loader.load(
+        'res/models/NEW ANGEL MODEL NO BACKPACK/no backpack.gltf',
+        function(gltf) {
+            console.log('Model loaded successfully');
+            model = gltf.scene;
+
+            // Log model structure without stringification
+            console.log('Model scene:', model);
+            console.log('Animations:', gltf.animations);
+
+            // Log children of the model
+            model.traverse((child) => {
+                console.log('Child:', child.type, child.name);
+                if (child.isMesh) {
+                    console.log('- Geometry:', child.geometry.type, 'Vertices:', child.geometry.attributes.position.count);
+                    console.log('- Material:', child.material.type);
+                }
+            });
+
+            // Add model to scene without any modifications
+            scene.add(model);
+
+            // Center the model
+            const box = new THREE.Box3().setFromObject(model);
+            const center = box.getCenter(new THREE.Vector3());
+            model.position.sub(center);
+
+            // Scale the model
+            const scaleAdjustment = 6;
+            model.scale.multiplyScalar(scaleAdjustment);
+
+            model.position.y -= 0.11;
+
+            // Apply material adjustments
+            adjustModelMaterials();
+
+            // Set up animation if available
+            if (gltf.animations && gltf.animations.length > 0) {
+                mixer = new THREE.AnimationMixer(model);
+                const action = mixer.clipAction(gltf.animations[0]);
+                action.play();
+            }
+
+            // Remove loading text
+            if (loadingText) {
+                scene.remove(loadingText);
+            }
+
+            // Dispatch a custom event to trigger the text reveal
+            window.dispatchEvent(new Event('modelLoaded'));
+
+            // Force a render after model is loaded
+            effect.render(scene, camera);
+        },
+        function(xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function(error) {
+            console.error('An error happened during model loading:', error);
+        }
+    );
+}
+
+function adjustModelMaterials() {
+    model.traverse((child) => {
+        if (child.isMesh) {
+            // Enable depth writing for transparent materials
+            if (child.material.transparent) {
+                child.material.depthWrite = true;
+            }
+            
+            // Disable frustum culling for all meshes
+            child.frustumCulled = false;
+            
+            // Optionally, adjust the rendering order
+            child.renderOrder = 1;
+        }
+    });
+}
