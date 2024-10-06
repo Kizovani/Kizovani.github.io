f50e60206010e75991a1cd849fb6e8acacdd53dc
Kizovani <perailaki@gmail.com>
2024-10-06 02:25:04 -0400

    added contact page

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         projects: resolve(__dirname, 'projects.html'),
-        info: resolve(__dirname, 'info.html')
+        info: resolve(__dirname, 'info.html'),
+        contact: resolve(__dirname, 'contact.html')
         // Add more pages as needed
       },
     },
